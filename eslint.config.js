053736d463a6e7cd8a60ef35df2deb3d7e8affca
Anthony Pena <anthony.pena@outlook.fr>
2024-09-17 10:13:44 +0200

    chore: eslint fix + config

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from 'typescript-eslint';
 
 export default [
     { files: ['src/**/*.{js,mjs,cjs,ts}'] },
-    { ignores: ['dist/**/*'] },
+    { ignores: ['dist/**/*', 'demo/web_modules/**/*'] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
