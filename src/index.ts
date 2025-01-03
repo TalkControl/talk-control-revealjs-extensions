0137fa490088e28a8b3603f10b37fb9988705cd7
jefbinomed <jean.francois.garreau@gmail.com>
2025-01-03 14:16:26 +0100

    core: fix feather icons load

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@ export * from './theme-initializer';
 export { default as Reveal } from 'reveal.js';
 
 export { fontAwesomeIconPack } from './icons-config/font-awesome-pack';
+
+export { featherIconPack } from './icons-config/feather-icons-pack';
+
 export { materialSymbolsIconPack } from './icons-config/material-symbols-pack';
