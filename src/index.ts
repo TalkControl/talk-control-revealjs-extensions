37b5b1f0dcd3d20056d64e2dfaffb88e63363539
jefbinomed <jean.francois.garreau@gmail.com>
2025-05-23 11:33:29 +0200

    ui: add tabs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import './index.scss';
 
-export * from './ui/tc-configurator-element';
+export * from './ui/tc-talk-control-element';
+export * from './ui/tc-revealjs-element';
 export * from './ui/tc-tree-slides-element';
+export * from './ui/tc-configurator-element';
 
 export * from './theme-initializer';
 
